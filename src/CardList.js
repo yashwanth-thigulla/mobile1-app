1cd68ecc2605a5cc296b50390669e67395c6adc8
yashwanth-thigulla <81611569+yashwanth-thigulla@users.noreply.github.com>
2021-04-20 14:00:01 +0530

    Update CardList.js

--- src/CardList.js
+++ src/CardList.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList=  ({robots})=>{
+    if(true){
+        throw new Error(<h1>Noooooo!</h1>);
+    }
     const cardcomponent = robots.map((user,i)=>{
         return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
     })
